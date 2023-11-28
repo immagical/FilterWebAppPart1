d2ed1b30376d9cbc10e3d4df37a5858d8ed95305
immagical <104602961+immagical@users.noreply.github.com>
2023-11-28 18:35:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(400,300);
+    canvas.center(); 
+}
+function draw () {
+
+}
+function takeSnapshot() {
+    save("FilterWebApp"); 
+}
